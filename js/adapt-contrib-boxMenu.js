79be439173733dd69bb2e2a448015c50f8206d3a
Guy Willis <guy.willis@kineo.com>
2018-12-17 11:24:31 +0000

    Naming refactor, split js into two files

--- js/adapt-contrib-boxMenu.js
+++ js/adapt-contrib-boxMenu.js
@@ -1,12 +1,13 @@
 define([
   'core/js/adapt',
-  'core/js/views/menuView'
-], function(Adapt, MenuView) {
+  'core/js/views/menuView',
+  "./adapt-contrib-boxMenuItemView"
+], function(Adapt, MenuView, BoxMenuItemView) {
 
   var BoxMenuView = MenuView.extend({
 
     className: function() {
-      return MenuView.prototype.className.apply(this) + " boxmenu-menu";
+      return MenuView.prototype.className.apply(this) + " boxmenu";
     },
 
     attributes: function() {
@@ -24,72 +25,18 @@ define([
           this.$('.js-children').append(new BoxMenuItemView({model: item}).$el);
         }
 
-        if(item.get('_isHidden')) {
+        if (item.get('_isHidden')) {
           item.set('_isReady', true);
         }
       });
     }
 
   }, {
-    template: 'boxmenu'
-  });
-
-  var BoxMenuItemView = MenuView.extend({
-
-    events: {
-      'click button' : 'onClickMenuItemButton'
-    },
-
-    attributes: function() {
-      return MenuView.prototype.resultExtend('attributes', {
-        'role': 'listitem',
-        'aria-labelledby': this.model.get('_id') + '-heading'
-      }, this);
-    },
-
-    className: function() {
-      var nthChild = this.model.get('_nthChild');
-      return [
-        'menu-item',
-        'menu-item-' + this.model.get('_id') ,
-        this.model.get('_classes'),
-        this.model.get('_isVisited') ? 'visited' : '',
-        this.model.get('_isComplete') ? 'completed' : '',
-        this.model.get('_isLocked') ? 'locked' : '',
-        'nth-child-' + nthChild,
-        nthChild % 2 === 0 ? 'nth-child-even' : 'nth-child-odd'
-      ].join(' ');
-    },
-
-    preRender: function() {
-      this.model.checkCompletionStatus();
-      this.model.checkInteractionCompletionStatus();
-    },
-
-    postRender: function() {
-      var graphic = this.model.get('_graphic');
-      if (graphic && graphic.src) {
-        this.$el.imageready(this.setReadyStatus.bind(this));
-        return;
-      }
-
-      this.setReadyStatus();
-    },
-
-    onClickMenuItemButton: function(event) {
-      if(event && event.preventDefault) event.preventDefault();
-      if(this.model.get('_isLocked')) return;
-      Backbone.history.navigate('#/id/' + this.model.get('_id'), {trigger: true});
-    }
-
-  }, {
-    template: 'boxmenu-item'
+    template: 'boxMenu'
   });
 
   Adapt.on('router:menu', function(model) {
-
     $('#wrapper').append(new BoxMenuView({model: model}).$el);
-
   });
 
 });
---
+++ js/adapt-contrib-boxMenuItemView.js
@@ -0,0 +1,64 @@
+define([
+  "core/js/adapt",
+  "core/js/views/menuView"
+], function(Adapt, MenuView) {
+
+  var BoxMenuItemView = MenuView.extend({
+
+    events: {
+      'click .js-btn-click' : 'onClickMenuItemButton'
+    },
+
+    attributes: function() {
+      return MenuView.prototype.resultExtend('attributes', {
+        'role': 'listitem',
+        'aria-labelledby': this.model.get('_id') + '-heading'
+      }, this);
+    },
+
+    className: function() {
+      var nthChild = this.model.get('_nthChild');
+      return [
+        'boxmenu__item',
+        'boxmenu__item-' + this.model.get('_id') ,
+        this.model.get('_classes'),
+        this.model.get('_isVisited') ? 'is-visited' : '',
+        this.model.get('_isComplete') ? 'is-completed' : '',
+        this.model.get('_isLocked') ? 'is-locked' : '',
+        'nth-child-' + nthChild,
+        nthChild % 2 === 0 ? 'nth-child-even' : 'nth-child-odd'
+      ].join(' ');
+    },
+
+    preRender: function() {
+      this.model.checkCompletionStatus();
+      this.model.checkInteractionCompletionStatus();
+    },
+
+    postRender: function() {
+      // var graphic = this.model.get('_graphic');
+      // if (graphic && graphic.src) {
+      //   this.$el.imageready(this.setReadyStatus.bind(this));
+      //   return;
+      // }
+
+      // this.setReadyStatus();
+
+      this.$el.imageready(_.bind(function() {
+        this.setReadyStatus();
+      }, this));
+    },
+
+    onClickMenuItemButton: function(event) {
+      if (event && event.preventDefault) event.preventDefault();
+      if (this.model.get('_isLocked')) return;
+      Backbone.history.navigate('#/id/' + this.model.get('_id'), {trigger: true});
+    }
+
+  }, {
+    template: 'boxMenuItem'
+  });
+
+  return BoxMenuItemView;
+
+});
