0ee0eb48e15ab2c7609d954f85494b05290b2f9e
Guy Willis <guy.willis@kineo.com>
2019-02-28 09:59:13 +0000

    Utilised updated menuView + menuItemView

--- js/adapt-contrib-boxMenu.js
+++ js/adapt-contrib-boxMenu.js
@@ -6,32 +6,9 @@ define([
 
   var BoxMenuView = MenuView.extend({
 
-    className: function() {
-      return MenuView.prototype.className.apply(this) + " boxmenu";
-    },
-
-    attributes: function() {
-      return MenuView.prototype.resultExtend('attributes', {
-        'role': 'main',
-        'aria-labelledby': this.model.get('_id')+'-heading'
-      }, this);
-    },
-
-    postRender: function() {
-      var nthChild = 0;
-      this.model.getChildren().each(function(item) {
-        if (item.get('_isAvailable') && !item.get('_isHidden')) {
-          item.set('_nthChild', ++nthChild);
-          this.$('.js-children').append(new BoxMenuItemView({model: item}).$el);
-        }
-
-        if (item.get('_isHidden')) {
-          item.set('_isReady', true);
-        }
-      });
-    }
-
   }, {
+    childView: BoxMenuItemView,
+    className: 'boxmenu',
     template: 'boxMenu'
   });
 
