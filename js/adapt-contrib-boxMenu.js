467d3353eb9278f110e1cad796737166fcf308d7
Oliver Foster <oliver.foster@kineo.com>
2022-09-06 10:18:40 +0100

    Fix: Remove deprecated behaviour (fixes #124) (#125)

--- js/adapt-contrib-boxMenu.js
+++ js/adapt-contrib-boxMenu.js
@@ -1,4 +1,4 @@
-import Adapt from 'core/js/adapt';
+import components from 'core/js/components';
 import MenuModel from 'core/js/models/menuModel';
 import BoxMenuView from './BoxMenuView';
 
@@ -6,12 +6,12 @@ import BoxMenuView from './BoxMenuView';
 // Note: This is necessary to maintain legacy behaviour in the AAT where
 // only one menu is usable per course and the course / menu is assumed to be
 // a core model and use the only installed MenuView.
-Adapt.register('course menu', {
+components.register('course menu', {
   view: BoxMenuView
 });
 
 // Use for "_component": "boxMenu", or "_view": "boxMenu" and "_model": "boxMenu"
-Adapt.register('boxMenu', {
+components.register('boxMenu', {
   view: BoxMenuView,
   model: MenuModel.extend({})
 });
