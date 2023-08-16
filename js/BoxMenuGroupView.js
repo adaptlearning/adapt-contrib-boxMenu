63fa8842495e43dddd96b4272e625af6ec4712f5
kirsty-hames <kirsty-hames@users.noreply.github.com>
2023-08-16 15:32:44 +0100

    Update item count for grouped items

--- js/BoxMenuGroupView.js
+++ js/BoxMenuGroupView.js
@@ -11,6 +11,19 @@ class BoxMenuGroupView extends MenuItemView {
     _.defer(this.addChildren.bind(this));
     this.$el.imageready(this.setReadyStatus.bind(this));
     this.$el.parents('.boxmenu__item-container').addClass('has-groups');
+    this.updateItemCount();
+  }
+
+  updateItemCount() {
+    let nthChild = 0;
+    const models = this.model.getChildren().models;
+    models.forEach(model => {
+      nthChild++;
+      model.set({
+        _nthChild: nthChild,
+        _totalChild: models.length
+      });
+    });
   }
 }
 
