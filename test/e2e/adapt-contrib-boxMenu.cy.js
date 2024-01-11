06afe5ff77eb52813176a2099bb6922eaf45c020
cahirodoherty-learningpool <cahir.odoherty@learningpool.com>
2024-01-11 12:16:22 +0000

    Wrap menu item tests in a per-language loop and reference the course build

--- test/e2e/adapt-contrib-boxMenu.cy.js
+++ test/e2e/adapt-contrib-boxMenu.cy.js
@@ -1,27 +1,38 @@
-import Content from '../../../src/course/en/contentObjects.json'
-import Course from '../../../src/course/en/course.json'
-
 describe('Menu Page', () => {
   beforeEach(() => {
+    cy.getBuild();
     cy.visit('/');
-  })
-
-  it(`should have the title '${Course.displayTitle}' and correct description`, () => {
-    cy.get('.menu__title-inner').should('contain', Course.displayTitle);
-    cy.get('.menu__body-inner').should('contain', Course.body);
   });
 
-  it(`should display ${Content.length} menu tiles`, () => {
-    cy.get('.menu-item').should('have.length', Content.length)
+  it('should have the correct title and description for every language', () => {
+    this.build?.availableLanguageNames?.forEach(lang => {
+      cy.getData(lang).then(data => {
+        it(`should have the title '${data.course.displayTitle}' and correct description for language '${lang}'`, () => {
+          cy.get('.menu__title-inner').should('contain', data.course.displayTitle);
+          cy.get('.menu__body-inner').should('contain', data.course.body);
+        });
+      });
+    });
+  });
 
-    cy.get('.menu-item').each(($item, index) => {
-      cy.get($item).within(() => {
-        cy.get('.menu-item__title').should('contain', Content[index].displayTitle)
-        cy.get('.menu-item__body').should('contain', Content[index].body)
-        cy.get('button').should('contain', Content[index].linkText)
-        cy.get('.menu-item__duration').should('contain', Content[index].duration)
-        cy.get('img.menu-item__image').should('exist').should('have.attr', 'src', Content[index]._graphic.src)
-      })
-    })
-  })
+  it('should display the correct menu tiles for every language', () => {
+    this.build?.availableLanguageNames?.forEach(lang => {
+      cy.getData(lang).then(data => {
+        it(`should display ${data.contentObjects.length} menu tiles for language '${lang}'`, () => {
+          cy.get('.menu-item').should('have.length', data.contentObjects.length)
+        });
+
+        it(`should display the correct information in the tile for language '${lang}'`, () => {
+          cy.get('.menu-item').each(($item, index) => {
+            cy.get($item).within(() => {
+              cy.get('.menu-item__title').should('contain', data.contentObjects[index].displayTitle)
+              cy.get('.menu-item__body').should('contain', data.contentObjects[index].body)
+              cy.get('button').should('contain', data.contentObjects[index].linkText)
+              cy.get('.menu-item__duration').should('contain', data.contentObjects[index].duration)
+              cy.get('img.menu-item__image').should('exist').should('have.attr', 'src', data.contentObjects[index]._graphic.src)
+            });
+          });
+        });
+      });
+    });
 });
