fed47b90116200bf83db78ac4abe8a461e95388e
cahirodoherty-learningpool <cahir.odoherty@learningpool.com>
2024-01-16 15:40:16 +0000

    Fixing paste fail

--- test/e2e/adapt-contrib-boxMenu.cy.js
+++ test/e2e/adapt-contrib-boxMenu.cy.js
@@ -13,24 +13,21 @@ describe('Menu Page', () => {
     });
   });
 
-  it(`should display ${this.data.contentObjects.length} menu tiles`, () => {
-    const { contentObjects } = this.data;
+  it(`should display the correct number (${this.data.contentObjects.length}) of menu tiles`, () => {
+    cy.get('.menu-item').should('have.length', this.data.contentObjects.length);
+  });
 
-    it(`should display ${contentObjects.length} menu tiles`, () => {
-      cy.get('.menu-item').should('have.length', contentObjects.length);
-    });
+  it(`should display the correct information in each tile`, () => {
+    cy.get('.menu-item').each(($item, index) => {
+      cy.get($item).within(() => {
+        const { _graphic, body, displayTitle, duration, linkText } = this.data.contentObjects[index];
 
-    it(`should display the correct information in the tile`, () => {
-      cy.get('.menu-item').each(($item, index) => {
-        cy.get($item).within(() => {
-          const { _graphic, body, displayTitle, duration, linkText } = contentObjects[index];
-
-          cy.get('.menu-item__title').should('contain', displayTitle);
-          cy.get('.menu-item__body').should('contain', body);
-          cy.get('button').should('contain', linkText);
-          cy.get('.menu-item__duration').should('contain', duration);
-          cy.get('img.menu-item__image').should('exist').should('have.attr', 'src', _graphic.src);
-        });
+        cy.get('.menu-item__title').should('contain', displayTitle);
+        cy.get('.menu-item__body').should('contain', body);
+        cy.get('button').should('contain', linkText);
+        cy.get('.menu-item__duration').should('contain', duration);
+        cy.get('img.menu-item__image').should('exist').should('have.attr', 'src', _graphic.src);
       });
     });
+  });
 });
