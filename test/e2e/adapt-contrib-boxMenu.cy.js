fda0ed1b3cb622623a8d61c63447290869c8c8c1
cahirodoherty-learningpool <cahir.odoherty@learningpool.com>
2024-01-16 14:23:31 +0000

    Making use of 'data' attribute that is already wrapped

--- test/e2e/adapt-contrib-boxMenu.cy.js
+++ test/e2e/adapt-contrib-boxMenu.cy.js
@@ -1,13 +1,11 @@
 describe('Menu Page', () => {
   beforeEach(() => {
-    cy.getData().then(data => {
-      this.data = data;
-    });
+    cy.getData();
     cy.visit('/');
   });
 
   it(`should have the title '${this.data.course.displayTitle}' and correct description`, () => {
-    const { body, displayTitle } = this.data.course
+    const { body, displayTitle } = this.data.course;
 
     it(`should have the title '${displayTitle}' and correct description`, () => {
       cy.get('.menu__title-inner').should('contain', displayTitle);
@@ -19,19 +17,19 @@ describe('Menu Page', () => {
     const { contentObjects } = this.data;
 
     it(`should display ${contentObjects.length} menu tiles`, () => {
-      cy.get('.menu-item').should('have.length', contentObjects.length)
+      cy.get('.menu-item').should('have.length', contentObjects.length);
     });
 
     it(`should display the correct information in the tile`, () => {
       cy.get('.menu-item').each(($item, index) => {
         cy.get($item).within(() => {
-          const { _graphic, body, displayTitle, duration, linkText } = contentObjects[index]
+          const { _graphic, body, displayTitle, duration, linkText } = contentObjects[index];
 
-          cy.get('.menu-item__title').should('contain', displayTitle)
-          cy.get('.menu-item__body').should('contain', body)
-          cy.get('button').should('contain', linkText)
-          cy.get('.menu-item__duration').should('contain', duration)
-          cy.get('img.menu-item__image').should('exist').should('have.attr', 'src', _graphic.src)
+          cy.get('.menu-item__title').should('contain', displayTitle);
+          cy.get('.menu-item__body').should('contain', body);
+          cy.get('button').should('contain', linkText);
+          cy.get('.menu-item__duration').should('contain', duration);
+          cy.get('img.menu-item__image').should('exist').should('have.attr', 'src', _graphic.src);
         });
       });
     });
