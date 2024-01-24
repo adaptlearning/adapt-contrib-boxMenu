c6052410822ff6a5eab594118622066a644f037d
Cahir O'Doherty <41006337+cahirodoherty-learningpool@users.noreply.github.com>
2024-01-24 11:32:48 +0000

    Wrap menu item tests in a per-language loop and reference the course build (#173)
    * Wrap menu item tests in a per-language loop and reference the course build
    
    * Removing language looping from tests
    
    * Destructuring some variables
    
    * Making use of 'data' attribute that is already wrapped
    
    * Fixing paste fail
    
    * Removing duplication
    
    * Performing empty value check and testing for non-writing of element in such cases

--- test/e2e/adapt-contrib-boxMenu.cy.js
+++ test/e2e/adapt-contrib-boxMenu.cy.js
@@ -1,27 +1,53 @@
-import Content from '../../../src/course/en/contentObjects.json'
-import Course from '../../../src/course/en/course.json'
-
 describe('Menu Page', () => {
   beforeEach(() => {
+    cy.getData();
     cy.visit('/');
-  })
-
-  it(`should have the title '${Course.displayTitle}' and correct description`, () => {
-    cy.get('.menu__title-inner').should('contain', Course.displayTitle);
-    cy.get('.menu__body-inner').should('contain', Course.body);
   });
 
-  it(`should display ${Content.length} menu tiles`, () => {
-    cy.get('.menu-item').should('have.length', Content.length)
+  it(`should have the title '${this.data.course.displayTitle}' and correct description`, () => {
+    const { body, displayTitle } = this.data.course;
 
+    cy.get('.menu__title-inner').should('contain', displayTitle);
+    cy.get('.menu__body-inner').should('contain', body);
+  });
+
+  it(`should display the correct number (${this.data.contentObjects.length}) of menu tiles`, () => {
+    cy.get('.menu-item').should('have.length', this.data.contentObjects.length);
+  });
+
+  it('should display the correct information in each tile', () => {
     cy.get('.menu-item').each(($item, index) => {
       cy.get($item).within(() => {
-        cy.get('.menu-item__title').should('contain', Content[index].displayTitle)
-        cy.get('.menu-item__body').should('contain', Content[index].body)
-        cy.get('button').should('contain', Content[index].linkText)
-        cy.get('.menu-item__duration').should('contain', Content[index].duration)
-        cy.get('img.menu-item__image').should('exist').should('have.attr', 'src', Content[index]._graphic.src)
-      })
-    })
-  })
+        const { _graphic, body, displayTitle, duration, linkText } = this.data.contentObjects[index];
+
+        if (_graphic?.src) {
+          cy.get('img.menu-item__image').should('exist').should('have.attr', 'src', _graphic.src);
+        } else {
+          cy.get('img.menu-item__image').should('not.exist');
+        }
+
+        if (displayTitle) {
+          cy.get('.menu-item__title').should('contain', displayTitle);
+        } else {
+          cy.get('.menu-item__title').should('not.exist');
+        }
+
+        cy.get('menu-item__details-inner').should('contain', _graphic.alt);
+
+        if (body) {
+          cy.get('.menu-item__body').should('contain', body);
+        } else {
+          cy.get('.menu-item__body').should('not.exist');
+        }
+
+        if (duration) {
+          cy.get('.menu-item__duration').should('contain', duration);
+        } else {
+          cy.get('.menu-item__duration').should('not.exist');
+        }
+
+        cy.get('button.boxmenu-item__button').should('contain', linkText);
+      });
+    });
+  });
 });
