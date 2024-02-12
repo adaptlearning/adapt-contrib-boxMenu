87fdf2f0b2d8c7fd9b01c53d50fbfb0d293c3acf
cahirodoherty-learningpool <cahir.odoherty@learningpool.com>
2024-02-12 09:57:04 +0000

    Better menu declaration

--- test/e2e/adapt-contrib-boxMenu.cy.js
+++ test/e2e/adapt-contrib-boxMenu.cy.js
@@ -4,6 +4,10 @@ describe('Menu Page', function () {
       this.boxMenus = this.data.filter(item => {
         return (item._component === 'boxMenu' || item._view === 'boxMenu' || ['course', 'menu'].includes(item._type));
       });
+
+      this.boxMenus = this.data.filter(item => item._component === 'boxMenu' || item._view === 'boxMenu');
+      if (this.boxMenus.length) return;
+      this.boxMenus = this.data.filter(item => ['course', 'menu'].includes(item._type));
     });
 
     cy.visit('/');
