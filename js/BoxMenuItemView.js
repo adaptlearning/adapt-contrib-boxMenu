e794c5b9b075f3b04d5aac262b419e9545d53e41
Brad Simpson <brad.simpson@kineo.com>
2024-04-09 11:57:33 -0600

    Add _areEntireItemsClickable option, update documentation

--- js/BoxMenuItemView.js
+++ js/BoxMenuItemView.js
@@ -1,15 +1,52 @@
 import MenuItemView from 'core/js/views/menuItemView';
+import Adapt from 'core/js/adapt';
 import router from 'core/js/router';
 
 class BoxMenuItemView extends MenuItemView {
 
   className() {
-    return `${super.className()} boxmenu-item`;
+    const entireItemClickable = this.areEntireItemsClickable() ? 'is-entire-item-clickable' : '';
+    return `${super.className()} boxmenu-item ${entireItemClickable}`;
   }
 
   events() {
+    if (this.areEntireItemsClickable()) {
+      return {
+        click: 'onClickMenuItemButton'
+      };
+    } else {
+      return {
+        'click .js-btn-click': 'onClickMenuItemButton'
+      };
+    }
+  }
+
+  attributes() {
+    const globals = Adapt.course.get('_globals');
+    const ariaLabel = [];
+
+    if (this.model.get('_isComplete')) {
+      ariaLabel.push(globals._accessibility._ariaLabels.complete);
+    }
+
+    if (this.model.get('_isVisited')) {
+      ariaLabel.push(globals._accessibility._ariaLabels.visited);
+    }
+
+    if (this.model.get('_isOptional')) {
+      ariaLabel.push(globals._accessibility._ariaLabels.optional);
+    }
+
+    const data = this.model.toJSON();
+    const itemCount = Handlebars.compile(globals._menu._boxMenu.itemCount)(data);
+    ariaLabel.push(itemCount);
+
+    const parentAttributes = Object.getPrototypeOf(BoxMenuItemView.prototype).attributes.call(this);
+
     return {
-      'click .js-btn-click': 'onClickMenuItemButton'
+      ...parentAttributes,
+      role: 'link',
+      'aria-label': ariaLabel.join(' ')
     };
   }
 
@@ -17,6 +54,10 @@ class BoxMenuItemView extends MenuItemView {
     if (event && event.preventDefault) event.preventDefault();
     if (this.model.get('_isLocked')) return;
     router.navigateToElement(this.model.get('_id'));
+  }
+
+  areEntireItemsClickable() {
+    return Adapt.course.get('_boxMenu')?._areEntireItemsClickable;
   }
 }
 
