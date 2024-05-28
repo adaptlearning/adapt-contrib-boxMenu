5b10f60f5648fb5c0817ea4ad812d3555802a05b
kirsty-hames <kirsty-hames@users.noreply.github.com>
2024-05-28 10:28:55 +0100

    capitalise Menu to be consistent with schema
    Co-authored-by: Guy Willis <guywillis@users.noreply.github.com>

--- templates/boxMenu.jsx
+++ templates/boxMenu.jsx
@@ -18,7 +18,7 @@ export default function BoxMenu (props) {
   const _graphic = _boxMenu?._graphic;
 
   // set menu background image
-  const backgroundImages = _boxmenu?._backgroundImage;
+  const backgroundImages = _boxMenu?._backgroundImage;
   const backgroundImage = backgroundImages[`_${device.screenSize}`] ?? backgroundImages._small;
   // set menu background styles
   const styles = _boxmenu._backgroundStyles;
