bd848d16137078f33cb4d47d781cbd6b4700e780
Oliver Foster <oliver.foster@kineo.com>
2025-03-31 15:17:17 +0100

    Remove comment

--- js/BoxMenuModel.js
+++ js/BoxMenuModel.js
@@ -1,7 +1,5 @@
 import MenuModel from 'core/js/models/menuModel';
 
-/** @typedef {import("core/js/models/adaptModel").default} AdaptModel */
-
 export default class BoxMenuModel extends MenuModel {
 
   getTypeGroup() {
