fee691e4e8a80b7da0cfab8af1e735b61810bdc2
Brad Simpson <brad.simpson@kineo.com>
2025-10-23 12:56:35 -0600

    Set up new data values

--- js/BoxMenuItemView.js
+++ js/BoxMenuItemView.js
@@ -1,3 +1,4 @@
+import Adapt from 'core/js/adapt';
 import MenuItemView from 'core/js/views/menuItemView';
 import router from 'core/js/router';
 
@@ -11,6 +12,30 @@ class BoxMenuItemView extends MenuItemView {
     return {
       'click .js-btn-click': 'onClickMenuItemButton'
     };
+  }
+
+  initialize(...args) {
+    super.initialize(...args);
+    this.setUpViewData();
+  }
+
+  setUpViewData() {
+    const _globals = Adapt.course.get('_globals');
+    const _boxMenu = Adapt.course.get('_boxMenu');
+    const _priorityLabels = _boxMenu?._priorityLabels;
+    const _isOptional = this.model.get('_isOptional');
+
+    const optionalLabel = _globals?._accessibility?._ariaLabels?.optional;
+    const requiredLabel = _globals?._accessibility?._ariaLabels?.required;
+
+    const showPriorityWhenOptional = _priorityLabels?._showPriorityWhenOptional && _isOptional && optionalLabel;
+    const showPriorityWhenRequired = _priorityLabels?._showPriorityWhenRequired && !_isOptional && requiredLabel;
+
+    this.model.set({
+      showPriority: showPriorityWhenOptional || showPriorityWhenRequired,
+      priorityClass: _isOptional ? 'is-optional' : 'is-required',
+      priorityLabel: _isOptional ? optionalLabel : requiredLabel
+    });
   }
 
   onClickMenuItemButton(event) {
